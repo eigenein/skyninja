d18cc5593fc2e0d5c00feee13cf964e9ccce4e27
Pavel Perestoronin <eigenein@gmail.com>
2014-03-11 02:30:48 +0300

    Use Docopt

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using DocoptNet;
+
+using SkyNinja.Core.Helpers;
 
 namespace SkyNinja.Cli
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        private const String Usage = @"
+SkyNinja Command Line Interface
+
+Usage:
+    cli --version
+        ";
+
+        public static void Main(String[] args)
         {
+            IDictionary<string, ValueObject> arguments = new Docopt().Apply(
+                Usage, args, version: AssemblyVersion.Current, exit: true);
         }
     }
 }
---
+++ Core/Helpers/AssemblyVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace SkyNinja.Core.Helpers
+{
+    public static class AssemblyVersion
+    {
+        private static readonly Version current = 
+            Assembly.GetExecutingAssembly().GetName().Version;
+
+        public static Version Current
+        {
+            get
+            {
+                return current;
+            }
+        }
+    }
+}
