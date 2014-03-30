f7614d4a6b4fd86b09b5377e1e8c5d0b0a492337
Pavel Perestoronin <eigenein@gmail.com>
2014-03-30 23:31:27 +0300

    #13 Group class tests

---
+++ Core.Test/Classes/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace SkyNinja.Core.Test.Classes
+{
+    [TestFixture]
+    public class Group
+    {
+        [TestCase("1", "2", false)]
+        [TestCase("3", "3", true)]
+        public void TestEquals(string group1, string group2, bool equals)
+        {
+            Assert.AreEqual(
+                equals, 
+                new Core.Classes.Group(group1).Equals(new Core.Classes.Group(group2)));
+        }
+    }
+}
--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Core")]
@@ -13,3 +14,14 @@ using System.Runtime.InteropServices;
 [assembly: Guid("C9EF5673-DB98-4B23-A96A-A4DAF4882003")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: InternalsVisibleTo(
+    "Core, PublicKey=0024000004800000940000" +
+    "00060200000024000052534131000400000100" +
+    "0100816335cdef184504dbfbe1d799f59a03a3" +
+    "94efd6de9905df6571e37161ef719d8bfc74ef" +
+    "e0843ccfc8c18910af823405017319ceab0553" +
+    "9211aa078411a418bcaa27d78e8cd5ed38db67" +
+    "36754696846e38182281753533342c806e44b9" +
+    "16eb17c0e1705677538de39a498b4612a1abd4" +
+    "da93a1615e8a9f46c7a8c0404a65fcb8")]
